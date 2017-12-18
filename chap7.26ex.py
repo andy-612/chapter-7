48db514a13105a7b07d4c2affad9ab4a196db14a
Andy Liu <andyliu@Andys-MacBook-Pro.local>
2017-12-17 20:05:24 -0500

    chapter 7 1-7 ex

--- chap7.26ex.py
+++ chap7.26ex.py
@@ -13,9 +13,55 @@ def even_sum(numlist):
             even=even+i
     return even
 
+letterlist={"I", "name", "paper", "food"}
+
 def odd_sum(numlist):
     odd=0
     for i in numlist:
         if i%2 !=0:
             odd=odd+i
     return odd
+
+def fiveletter_words(letterlist):
+    sum= 0
+    for i in letterlist:
+        if len(i) == 5:
+            sum = sum+1
+    return sum
+
+def sumupto_even(numlist):
+    sum = 0
+    for i in numlist:
+        if i % 2 == 0:
+            break
+        else:
+            sum+=i
+    return sum
+
+namelist={"George", "James", "Sam"}
+def upto_sam(namelist):
+    sum= 0
+    for i in namelist:
+        if i == "sam":
+            sum = sum+1
+            break
+        sum = sum+1
+    return sum
+
+
+def newtsqrt(n):
+    approx = n/2
+    while True:
+        better = (approx + n/approx)/2
+        if abs(better - approx) < .001:
+            return better
+        approx = better
+        
+        
+        
+def is_prime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return "not prime"
+        else:
+            return "prime"
